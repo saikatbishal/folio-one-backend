d3f56fb62358679843f6f702c9a99c444185c396
SAIKAT BISHAL <54259560+saikatbishal@users.noreply.github.com>
2025-11-18 10:05:56 +0530

    added whitelist url

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:4173",
+      "https://folio-one-brown.vercel.app/",
       "https://folio-one-saikatbishals-projects.vercel.app",
       "https://folio-one-git-master-saikatbishals-projects.vercel.app",
       "https://folio-oqpsdkssg-saikatbishals-projects.vercel.app",
